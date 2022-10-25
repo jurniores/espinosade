f9158c9a0aa6234edbacf60b692883ebfe01dc7a
jurniores <65627649+jurniores@users.noreply.github.com>
2022-10-25 16:50:50 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var ph2 = document.querySelector(".ph2")
+var sh2 = document.querySelector(".sh2")
+var input = document.querySelector("input")
+var btn = document.querySelector("button")
+
+btn.addEventListener("click", Converte);
+
+
+
+function Converte(e){
+    let val = input.value;
+    if(val.length <= 0) return;
+    ph2.innerHTML = (val*0,675)/100+"g"
+    sh2.innerHTML = (val/2)/100+" Qtd";
+    input.value = "";
+}
+
+
+
